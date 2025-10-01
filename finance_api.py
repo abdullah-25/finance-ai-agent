688ce8a91d0bed7c0fac513973115067e1a0efd7
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2025-09-30 21:47:03 -0400

    add finance_api skeleton code

---
+++ finance_api.py
@@ -0,0 +1,22 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = "https://api.dataforseo.com/v3/serp/google/finance_markets/live/advanced"
+
+payload="[{\"location_code\":2124, \"language_code\":\"en\", \"market_type\":\"indexes/americas\"}]"
+# Get the base64 encoded credentials from environment variables
+finance_api_base64 = os.getenv('FINANCE_API_BASE64')
+if not finance_api_base64:
+    raise ValueError('FINANCE_API_BASE64 environment variable is not set')
+
+headers = {
+    'Authorization': f'Basic {finance_api_base64}',
+    'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
